042bc9bee1b3c38e740b61b6b1cd3e24ded6a458
David Glick <david@glicksoftware.com>
2013-10-08 14:08:19 -0700

    vb

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.3'
+version = '1.4dev'
 
 setup(
     name = 'plone.recipe.alltests',
