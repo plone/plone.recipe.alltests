e91248d8af6e892645935df39d2e86e53916247f
Maurits van Rees <maurits@vanrees.org>
2016-02-09 21:36:18 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,34 +12,39 @@ setup(
     version=version,
     author="Hanno Schlichting",
     author_email="hanno@hannosch.eu",
-    description="Buildout recipe for running tests isolated at package boundaries",
+    description=("Buildout recipe for running tests isolated at package "
+                 "boundaries"),
     long_description=(
         read('README.rst')
         + '\n' +
         read('CHANGES.rst')
     ),
-    license = "ZPL 2.1",
-    keywords = "zope2 buildout",
-    url='http://pypi.python.org/pypi/plone.recipe.alltests',
+    license="ZPL 2.1",
+    keywords="zope2 buildout",
+    url='https://pypi.python.org/pypi/plone.recipe.alltests',
     classifiers=[
         "License :: OSI Approved :: Zope Public License",
         "Framework :: Buildout",
         "Framework :: Plone",
+        "Framework :: Plone : 4.3",
+        "Framework :: Plone : 5.0",
         "Framework :: Zope2",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
     ],
     tests_require=['zope.testing'],
-    packages = find_packages('src'),
-    include_package_data = True,
+    packages=find_packages('src'),
+    include_package_data=True,
     package_dir={'': 'src'},
-    namespace_packages = ['plone', 'plone.recipe'],
-    install_requires = [
+    namespace_packages=['plone', 'plone.recipe'],
+    install_requires=[
         'zc.buildout',
         'setuptools',
         'zc.recipe.egg',
     ],
     zip_safe=False,
-    entry_points = {
+    entry_points={
         'zc.buildout': ['default = %s:Recipe' % 'plone.recipe.alltests']
     },
 )
